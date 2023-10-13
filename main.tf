d558d06c66dbc12b74805eab90b119e7df6cdecc
ko4gat <144299220+ko4gat@users.noreply.github.com>
2023-10-13 00:11:56 -0400

    40 terratowns provider (#45)
    * #40 wip setting up the skeleton for creating a custom terraform provider
    
    * #40 Implement terraform provider block in terraform
    
    * #40 Set up skeleton for resource
    
    * #40 Finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ terraform {
   }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -32,3 +32,22 @@ provider "terratowns" {
 #  error_html_filepath = var.error_html_filepath
 #  content_version = var.content_version
 #}
+
+#}
+
+resource "terratowns_home" "home" {
+  name = "Guild Wars 1!"
+  description = <<DESCRIPTION
+Guild Wars 1, released in 2005, was a popular MMORPG known for its engaging storyline,
+unique character customization with a dual profession system, 
+competitive Player vs. Player (PvP) battles, stunning graphics,
+and no monthly subscription fees. It offered cooperative missions and
+received expansions over time, adding new content and keeping the game exciting. 
+The combination of immersive lore, strategic gameplay, and accessible online play made 
+Guild Wars 1 a memorable and enjoyable experience for players.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
