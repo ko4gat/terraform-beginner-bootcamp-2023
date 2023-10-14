685c124b55dd9cbba4d71586ce7081e8fb16e320
ko4gat <144299220+ko4gat@users.noreply.github.com>
2023-10-14 01:03:51 -0400

    46 terratowns test (#47)
    * #46 WIP, issues with photo appearing as broken link on test server.
    
    * #46 Test terrahome against terratowns test town.

--- main.tf
+++ main.tf
@@ -19,24 +19,23 @@ terraform {
   }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
-#}
 
 resource "terratowns_home" "home" {
-  name = "Guild Wars 1!"
+  name = "Guild Wars 1"
   description = <<DESCRIPTION
 Guild Wars 1, released in 2005, was a popular MMORPG known for its engaging storyline,
 unique character customization with a dual profession system, 
@@ -46,8 +45,8 @@ received expansions over time, adding new content and keeping the game exciting.
 The combination of immersive lore, strategic gameplay, and accessible online play made 
 Guild Wars 1 a memorable and enjoyable experience for players.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
-  content_version = 1
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "asd7f7v6.cloudfront.net"
+  town = "missingo"
+  content_version = 5
 }
